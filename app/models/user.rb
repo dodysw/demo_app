6cc17ee9f20f8b005e9d2977b1b8adc04258ff93
Dody Suria Wijaya <dodysw@gmail.com>
2013-12-16 12:50:46 +0700

    Exercise 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     #has_many :microposts
-    before_save { self.email = email.downcase  }
+    before_save { email.downcase!  }
     validates :name, presence: true, length: { maximum: 50 }
     validates :password, length: { minimum: 6 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)+[a-z]+\z/i
