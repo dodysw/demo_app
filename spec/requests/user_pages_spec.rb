fda4761e74ac485871b6ff8ef4f9e56892bc794d
Dody Suria Wijaya <dodysw@gmail.com>
2013-12-27 10:41:40 +0700

    Finish chapter 7 exercise

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "UserPages" do
   subject { page }
-  describe "signup page" do
+  describe "signup" do
     before { visit signup_path }
     let(:submit) { "Create my account" }
     
@@ -12,6 +12,11 @@ describe "UserPages" do
     describe "with invalid information" do
         it "should not create a user" do
             expect { click_button submit }.not_to change(User, :count)
+        end
+        describe "after submission" do
+            before { click_button submit }
+            it { should have_title('Sign up') }
+            it { should have_content('error') }
         end
     end
 
@@ -25,6 +30,13 @@ describe "UserPages" do
 
         it "should create a user" do
             expect { click_button submit }.to change(User, :count).by(1)
+        end
+        describe "after saving the user" do
+            before { click_button submit }
+            let(:user) { User.find_by(email: 'user@example.com') }
+
+            it { should have_title(user.name) }
+            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         end
     end
   end
